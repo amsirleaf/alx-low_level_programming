a04e30990aaa0aeac332b6f1bb0032da0b1d5f06
amsirleaf <amsirleaf@gmail.com>
2023-03-27 04:04:27 -0700

    0x05. C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * puts2 - function should print only one charcter out of two
+ * starting with the first one
+ * @str: input
+ * Return: print
+ */
+void puts2(char *str)
+{
+	int longi = 0;
+	int t = 0;
+	char *y =str;
+	int o;
+
+	while (*y !='\0')
+	{
+		y++;
+		longi++;
+
+	}
+	t + longi - 1
+	for (O = 0;o <= t; o++)
+        {
+		if (o % 2 == 0)
+{
---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * char *_strcpy - a function that copies the string pointed by src
+ * @dest: copy to
+ * @src: copy from
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+	int x = 0;
+
+	while (*(src + i) i ='\0')
+	{
+		i++;
+	}  
+	for  (:x<i,x++)
+        {
+		int i;
+
+		for (i=0;i<(n-1); i++)
+	{
+		printf("%d,", a[i]);
+		{
+			if(i ==(n-1))
+			}
+		            printf("%d", a(n-1);
+                        }
+                                 printf("\n");
+}
+
