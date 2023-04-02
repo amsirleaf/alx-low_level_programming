1ee196a649174ce2703508ce8230c6fc1700ceff
amsirleaf <amsirleaf@gmail.com>
2023-04-01 19:25:24 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,47 +1,115 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * print_times_table - prints the n times table, starting with 0
- * @n: number of the times table
+ * print_thousand - print out the digit in the thousand spot
+ * @sum: Value to scan for thousand spot
+ *
+ * Return: Void
  */
-void print_times_table(int n)
+static void print_thousand(int sum)
 {
-	int i, j, k;
-
-	if (n >= 0 && n <= 15)
+	if (sum / 1000 == 0)
 	{
-		for (i = 0; i <= n; i++)
+		_putchar(32);
+	}
+	else
+	{
+		_putchar('0' + (sum / 1000));
+	}
+}
+
+
+/**
+ * print_hundred - print out the digit in the hundred spot
+ * @sum: Value to scan for hundred spot
+ *
+ * Return: Void
+ */
+static void print_hundred(int sum)
+{
+	if (sum / 100 == 0)
+	{
+		_putchar(32);
+	}
+	else
+	{
+		_putchar('0' + (sum / 100));
+	}
+}
+
+
+/**
+ * print_tenth - print out the digit in the tenth spot
+ * @sum: Value to scan for tenth spot
+ *
+ * Return: Void
+ */
+static void print_tenth(int sum)
+{
+	if (sum / 10 == 0)
+	{
+		_putchar(32);
+	}
+	else
+	{
+		if ((sum / 10) > 9)
 		{
-			for (j = 0; j <= n; j++)
-			{
-				k = j * i;
-				if (j == 0)
-				{
-					_putchar(k + '0');
-				} else if (k < 10 && j != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
-				} else if (k >= 10 && k < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
-				} else if (k >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((k / 100) + '0');
-					_putchar(((k / 10) % 10) + '0');
-					_putchar((k % 10) + '0');
-				}
-			}
-			_putchar('\n');
+			_putchar('0' + (sum / 10 % 10));
+		}
+		else
+		{
+			_putchar('0' + (sum / 10));
 		}
 	}
 }
+
+
+/**
+ * print_base10 - print out the digit in the base10 spot
+ * @sum: Value to scan for base10 spot
+ *
+ * Return: Void
+ */
+static void print_base10(int sum)
+{
+	if (sum % 10 == 0)
+	{
+		_putchar('0');
+	}
+	else
+	{
+		_putchar('0' + (sum % 10));
+	}
+}
+
+/**
+ * print_times_table - print out the 9 times table
+ * @n: Valuse use to generate times tables.
+ *
+ * Return: void
+ */
+void print_times_table(int n)
+{
+	int v, h;
+
+	if (n > 15)
+	{
+		return;
+	}
+	for (h = 0 ; h < n + 1 ; h++)
+	{
+		_putchar('0');
+		for (v = 1 ; v < n + 1 ; v++)
+		{
+			int sum = v * h;
+
+			_putchar(',');
+			print_thousand(sum);
+			print_hundred(sum);
+			print_tenth(sum);
+			print_base10(sum);
+		}
+		_putchar('\n');
+	}
+}
+
