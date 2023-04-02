4efd4da96488d56937dd45905830aa71042f17e5
amsirleaf <amsirleaf@gmail.com>
2023-04-01 19:14:41 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,28 +1,35 @@
-#include "main.h"
-
+#include "holberton.h"
 /**
- * jack_bauer - prints every minute of the day of Jack Bauer
- * starting from 00:00 to 23:59
+ * jack_bauer - print every minute of the day, starting from 00:00 - 23:59.
+ *
+ * Return : void
  */
 void jack_bauer(void)
 {
-	int i, j;
+	int h0, h1;
+	int m0, m1;
 
-	i = 0;
-
-	while (i < 24)
+	for (h0 = 0 ; h0 <= 2 ; h0++)
 	{
-		j = 0;
-		while (j < 60)
+		for (h1 = 0 ; h1 < 10 ; h1++)
 		{
-			_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
-			_putchar(':');
-			_putchar((j / 10) + '0');
-			_putchar((j % 10) + '0');
-			_putchar('\n');
-			j++;
+			for (m0 = 0 ; m0 < 6 ; m0++)
+			{
+				for (m1 = 0 ; m1 < 10 ; m1++)
+				{
+					_putchar('0' + h0);
+					_putchar('0' + h1);
+					_putchar(':');
+					_putchar('0' + m0);
+					_putchar('0' + m1);
+					_putchar('\n');
+					if (h0 == 2 && h1 == 3 && m0 == 5 && m1 == 9)
+					{
+						return;
+					}
+				}
+			}
 		}
-		i++;
 	}
 }
+
